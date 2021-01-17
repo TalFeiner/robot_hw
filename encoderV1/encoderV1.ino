cd99932d7deb7395b2eff92ed10da5c2b7da5fe4
Or Tslil <ortslil64@gmail.com>
2021-01-17 11:39:24 +0200

    update

--- encoderV1/encoderV1.ino
+++ encoderV1/encoderV1.ino
@@ -1,25 +1,26 @@
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
+#include <Coordinates.h>
 
 //Mega 2, 3, 18, 19, 20, 21
 byte pinL1 = 2, pinL2 = 3; // 18
-byte pinR1 = 19, pinR2 = 20;  // 21
+byte pinR1 = 19, pinR2 = 21;  // 21
 long oldPositionL  = -999, oldPositionR  = -999;
 float D = 0.1651, wheelsSeparation = 0.36; //[m]
-int pulsesPerRev = 60;
+int pulsesPerRev = 60, duration = 2;
 double oldTime;
 
 struct kalman {
-   double vel_kf = 0.0;
-   double p_kf = 1.0;
+   double vel_kf;
+   double p_kf;
 };
 
 double rleftR_kf = 1.1, leftQ_kf = 0.5;
 double rightR_kf = 1.1, rightQ_kf = 0.5;
-struct kalman angularVelLKF;
-struct kalman angularVelRKF;
+struct kalman angularVelLKF = {vel_kf: 0.0, p_kf: 1.0};
+struct kalman angularVelRKF = {vel_kf: 0.0, p_kf: 1.0};
 
-double x, theta;
+double s, theta;
 
 Encoder encL(pinL1, pinL2);
 Encoder encR(pinR1, pinR2);
@@ -47,7 +48,7 @@ void setup() {
 void loop() {
 
   double dt = millis() - oldTime;
-  if (dt >= 10){
+  if (dt >= duration){
     oldTime = millis();
     dt = dt / 1000.0;  //[sec]
     
@@ -60,8 +61,8 @@ void loop() {
     oldPositionL = newPositionL;
     oldPositionR = newPositionR;
 
-    angularVelLKF = kalmanFunc (angularVelL, angularVelLKF, rleftR_kf, leftQ_kf);
-    angularVelRKF = kalmanFunc (angularVelR, angularVelRKF, rightR_kf, rightQ_kf);
+    angularVelLKF = kalmanFunc (-angularVelL, angularVelLKF, rleftR_kf, leftQ_kf);
+    angularVelRKF = kalmanFunc (-angularVelR, angularVelRKF, rightR_kf, rightQ_kf);
     
     double linearVelL = D / 2 * angularVelLKF.vel_kf; //[m/sec]
     double linearVelR = D / 2 * angularVelRKF.vel_kf;  //[m/sec]
@@ -69,8 +70,13 @@ void loop() {
     double omega = (linearVelR - linearVelL) / wheelsSeparation; //[rad/sec]
     double linearV = (linearVelR + linearVelL) / 2;  //[m/sec]
 
-    x = linearV * dt + x;
-    theta = omega * dt + theta;
+    s = linearV * dt + s;  //[m]
+    theta = omega * dt + theta;  //[rad]
+
+    Coordinates point = Coordinates();
+    point.fromPolar(s,theta);
+    float x = point.getX();  //[m]
+    float y = point.getY();  //[m]
       
      //---- debug -----//
 //    Serial.print("vel L: ");
